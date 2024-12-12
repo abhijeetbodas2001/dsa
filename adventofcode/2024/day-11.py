3d11035de0e4d2cbeb78649c96f3b944b935160a
Abhijeet Prasad Bodas <abhijeetbodas2001@gmail.com>
2024-12-12 10:52:49 +0530

    aoc

--- adventofcode/2024/day-11.py
+++ adventofcode/2024/day-11.py
@@ -1,4 +1,4 @@
-from collections import deque
+from collections import deque, Counter
 import time
 from copy import copy
 
@@ -38,46 +38,81 @@ def solve():
     print("Part 1", len(ll))
 
     # Part 2
+    # ll = list(map(int, arr.split(" ")))
+    # head = Node(ll[0], 0)
+    # prev = head
+    # for idx in range(1, len(ll)):
+    #     curr = Node(ll[idx], 0)
+    #     prev.next = curr
+    #     prev = curr
+    #
+    # print(time.time())
+    # curr = head
+    # ans = 0
+    # while curr:
+    #     while curr.proc < 75:
+    #         curr.proc += 1
+    #
+    #         if curr.val == 0:
+    #             curr.val = 1
+    #             continue
+    #
+    #         s = str(curr.val)
+    #         n = len(s)
+    #         if n % 2 == 0:
+    #             part1, part2 = int(s[: n // 2]), int(s[n // 2 :])
+    #             curr.val = part1
+    #             tmp = curr.next
+    #             new_node = Node(part2, curr.proc)
+    #             curr.next = new_node
+    #             new_node.next = tmp
+    #             continue
+    #
+    #         curr.val *= 2024
+    #
+    #     tmp = curr.next
+    #     curr.next = None
+    #     curr = tmp
+    #
+    #     ans += 1
+    #
+    # print("Part 2:", ans)
+    # print(time.time())
+
+    # Part 2
+    gives: dict[int, Counter] = {}  # map from value to counter
+    with open("./input.txt", "r") as f:
+        arr = f.read().splitlines()[0]
     ll = list(map(int, arr.split(" ")))
-    head = Node(ll[0], 0)
-    prev = head
-    for idx in range(1, len(ll)):
-        curr = Node(ll[idx], 0)
-        prev.next = curr
-        prev = curr
+    print(ll)
 
-    print(time.time())
-    curr = head
-    ans = 0
-    while curr:
-        while curr.proc < 75:
-            curr.proc += 1
+    def cache(num: int) -> dict[int, int]:
+        if num in gives:
+            return gives[num]
 
-            if curr.val == 0:
-                curr.val = 1
-                continue
+        g = {}
+        res = 0
+        s = str(num)
+        n = len(s)
+        if num == 0:
+            res = [1]
+        elif n % 2 == 0:
+            res = [int(s[: n // 2]), int(s[n // 2 :])]
+        else:
+            res = [num * 2024]
 
-            s = str(curr.val)
-            n = len(s)
-            if n % 2 == 0:
-                part1, part2 = int(s[: n // 2]), int(s[n // 2 :])
-                curr.val = part1
-                tmp = curr.next
-                new_node = Node(part2, curr.proc)
-                curr.next = new_node
-                new_node.next = tmp
-                continue
+        gives[num] = Counter(res)
+        return gives[num]
 
-            curr.val *= 2024
+    curr = Counter(ll)
+    for idx in range(75):
+        new = {}
+        for num, count in curr.items():
+            for x, y in cache(num).items():
+                new[x] = new.get(x, 0) + count * y
+        curr = new
 
-        tmp = curr.next
-        curr.next = None
-        curr = tmp
-
-        ans += 1
-
-    print("Part 2:", ans)
-    print(time.time())
+    print(sum(v for v in curr.values()))
 
 
 if __name__ == "__main__":
